32df9d1f3c8b90af4b649b3cc21ea13a1b26c337
Ibrahim Mohammed <ibrahom0780@gmail.com>
2026-09-12 21:26:45 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,10 +19,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await GoogleSignIn.instance.initialize(
-    serverClientId:
-        '528855485063-mjhi0vng1k0nqob5879j5nnstc93g9tb.apps.googleusercontent.com',
-  );
+  if (Platform.isAndroid) {
+    await GoogleSignIn.instance.initialize(
+      serverClientId:
+          '528855485063-mjhi0vng1k0nqob5879j5nnstc93g9tb.apps.googleusercontent.com',
+    );
+  }
+
   setupLocator();
   Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
