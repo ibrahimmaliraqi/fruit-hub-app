93ba63afd0c3b7298178e3aff813da63766d2d63
Ibrahim Mohammed <ibrahom0780@gmail.com>
2026-09-12 16:11:24 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub_app/core/router/app_router.dart';
 import 'package:fruit_hub_app/core/theme/app_colors.dart';
+import 'package:fruit_hub_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
