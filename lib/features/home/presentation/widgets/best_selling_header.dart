a40f18fb5522f740fe1922395609fa2a850d33b6
Ibrahim Mohammed <ibrahom0780@gmail.com>
2026-09-23 19:55:59 +0300

    BestSellingHeader

---
+++ lib/features/home/presentation/widgets/best_selling_header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class BestSellingHeader extends StatelessWidget {
+  const BestSellingHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'الأكثر مبيعًا',
+          textAlign: TextAlign.right,
+          style: TextStyle(
+            color: const Color(0xFF0C0D0D) /* Grayscale-950 */,
+            fontSize: 16,
+            fontFamily: 'Cairo',
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Spacer(),
+        Text(
+          'المزيد',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: const Color(0xFF949D9E) /* Grayscale-400 */,
+            fontSize: 13,
+            fontFamily: 'Cairo',
+            fontWeight: FontWeight.w400,
+            height: 1.60,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/features/home/presentation/widgets/best_selling_header.dart';
 import 'package:fruit_hub_app/features/home/presentation/widgets/home_app_bar.dart';
-import 'package:fruit_hub_app/features/home/presentation/widgets/home_banner.dart';
 import 'package:fruit_hub_app/features/home/presentation/widgets/home_banner_list.dart';
 import 'package:fruit_hub_app/features/home/presentation/widgets/home_search.dart';
 import 'package:gap/gap.dart';
@@ -19,6 +19,8 @@ class HomeViewBody extends StatelessWidget {
           HomeSearchField(),
           Gap(13),
           HomeBannerList(),
+          Gap(13),
+          BestSellingHeader(),
         ],
       ),
     );
